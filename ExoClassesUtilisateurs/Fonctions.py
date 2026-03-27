da98634f9a93345c62d5754f2c606b43b62e2d0a
MeganeCrampon <meganelulu@gmail.com>
2026-03-27 21:22:57 +0100

    Amélriorations des fonctions d'ajout utilisateur et authentification

--- ExoClassesUtilisateurs/Fonctions.py
+++ ExoClassesUtilisateurs/Fonctions.py
@@ -4,41 +4,55 @@ from Classe import Utilisateur
 user_base = []
 
 # LOGIQUE METIER
-def add_user_mdp():
-    nouveau_mdp = input(print("Veuillez maintenant entrer votre mot de passe : ")).strip()
-    if nouveau_mdp(len) < 8:
-        print("Mot de passe trop court.")
-    else:
-        nouveau_mdp_hash = hash(nouveau_mdp)
-        Utilisateur.mdp.append(nouveau_mdp_hash)
-
 def add_user():
-    reponse = input(print("Vous ne possedez pas de compte. Voulez vous en créer un ? (Y/N): ")).strip().upper()
-    if reponse == "Y":
-        nouveau_nom = input(print("Veuillez entrer votre nouveau nom d'utilisateur : ")).strip().capitalize()
-        Utilisateur.nom.append(nouveau_nom)
-        add_user_mdp()
-    elif reponse == "N":
-        print("Au revoir.")
-    else : 
-        print("Désolé, saisie incorrecte.")
+    print("\n--- CRÉATION DE COMPTE ---")
+    reponse = input("Vous ne possedez pas de compte. Voulez vous en créer un ? (Y/N): ").strip().upper()
+
+    if reponse != "Y":
+        print("Opération annulée.")
+        exit()
+    else :
+        while True :
+            nouveau_nom = input("Veuillez entrer un nom d'utilisateur : ").strip().capitalize()
+            if not nouveau_nom:
+                print("Le nom ne peut pas être vide.")
+                continue
+            if any (u.nom == nouveau_nom for u in user_base):
+                print("Ce nom d'utilisateur existe déjà. Veuillez choisir un autre nom.")
+                continue
+            break
+        while True :
+            nouveau_mdp = input("Veuillez maintenant entrer votre mot de passe : ").strip()
+            if len(nouveau_mdp) < 8:
+                print("Mot de passe trop court.")
+            else:
+                break
+        # CREATION OBJET et STOCKAGE en DB
+        nouvel_utilisateur = Utilisateur(nouveau_nom, nouveau_mdp)
+        user_base.append(nouvel_utilisateur)
+        print(f"Votre compte a bien été crée {nouveau_nom} !")
 
 def verif_mdp():
+    print(f"\nBonjour {Utilisateur.nom} !")
     essai = 0
-    mdp_entre = input(print("Veuillez maintenant entrer votre mot de passe : "))
+    mdp_entre = input("Veuillez entrer votre mot de passe : ")
     while True :
         if mdp_entre != Utilisateur.mdp:
             essai += 1
-            print("Accès refusé. Au bout de 3 essais le compte sera temporairement bloqué.")
+            print(f"Accès refusé. Au bout de 3 essais le compte sera temporairement bloqué. Essai {essai}/3.")
             if essai == 3:
-                print("Trop de tentatives. Votre compte est temporairement bloqué.")
+                print("\nTrop de tentatives. Votre compte est temporairement bloqué.")
                 break
         else :
             print(f"Accès autorisé. Bienvenue {Utilisateur.nom}.")
+            return
 
 def auth():
-    nom_entre = input(print("Veuillez entrer votre nom d'utilisateur : ")).strip().capitalize()
-    if nom_entre != Utilisateur.nom:
-        add_user()
+    print("\n---CONNEXION---")
+    nom_entre = input("Veuillez entrer votre nom d'utilisateur : ").strip().capitalize()
+    utilisateur_trouve = next((u for u in user_base if u.nom == nom_entre), None)
+    if utilisateur_trouve:
+        verif_mdp(utilisateur_trouve)
     else :
-        verif_mdp()
+        print("Nom d'utilisateur inconnu.")
+        add_user()
