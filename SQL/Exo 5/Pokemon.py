14f6dec8f4a1106f6e00d1f03a233d17ad9541e2
MeganeCrampon <meganelulu@gmail.com>
2026-04-02 16:49:44 +0200

    Essai export txt

--- SQL/Exo 5/Pokemon.py
+++ SQL/Exo 5/Pokemon.py
@@ -3,6 +3,7 @@ import sqlite3
 conn = sqlite3.connect("SQL/Exo 5/dresseurs_et_pokemons.db")
 cur = conn.cursor()
 
+# CREATION DES TABLES   
 cur.execute('''
 CREATE TABLE IF NOT EXISTS Pokemons (
     id_pk INTEGER PRIMARY KEY,
@@ -15,11 +16,26 @@ CREATE TABLE IF NOT EXISTS Pokemons (
 cur.execute('''
 CREATE TABLE IF NOT EXISTS Dresseurs (
     id_dr INTEGER PRIMARY KEY,
-    nom_dr TEXT UNIQUE,
+    nom_dr TEXT,
     pokemon_id_dr INTEGER,
     FOREIGN KEY (pokemon_id_dr) REFERENCES Pokemons(id_pk)
 )''')
 
+# CREATION D'UNE VUE POUR LES EQUIPES
+def add_actu_equipe(nom_dr, pokemon_id_dr):
+    cur.execute("INSERT INTO Dresseurs (nom_dr, pokemon_id_dr) VALUES (?, ?)", 
+                (nom_dr, pokemon_id_dr))
+    cur.execute("DROP VIEW IF EXISTS Equipe") 
+    cur.execute('''
+        CREATE VIEW Equipe AS
+        SELECT Dresseurs.nom_dr, Pokemons.nom_pk AS nom_pokemon
+        FROM Dresseurs
+        JOIN Pokemons ON Dresseurs.pokemon_id_dr = Pokemons.id_pk
+    ''')
+    conn.commit()
+    print(f"L'équipe du dresseur {nom_dr} a été mise à jour !")
+
+# AJOUTS MANUELS AUX TABLES
 cur.execute("SELECT COUNT(*) FROM Pokemons")
 nombre_pokemons = cur.fetchone()[0]
 if nombre_pokemons == 0:
@@ -34,25 +50,12 @@ cur.execute("SELECT COUNT(*) FROM Dresseurs")
 nombre_dresseurs = cur.fetchone()[0]
 if nombre_dresseurs == 0:
     cur.execute("INSERT OR REPLACE INTO Dresseurs (nom_dr, pokemon_id_dr) VALUES ('Sacha', 1)")
-    cur.execute("INSERT OR REPLACE INTO Dresseurs (nom_dr, pokemon_id_dr) VALUES ('Sacha', 5)")
     cur.execute("INSERT OR REPLACE INTO Dresseurs (nom_dr, pokemon_id_dr) VALUES ('Régis', 2)")
     cur.execute("INSERT OR REPLACE INTO Dresseurs (nom_dr, pokemon_id_dr) VALUES ('Pierre', 3)")
     cur.execute("INSERT OR REPLACE INTO Dresseurs (nom_dr, pokemon_id_dr) VALUES ('Cynthia', 4)")
     conn.commit()
 
-def add_actu_equipe(id_dr, nom_dr, id_pk, nom_pk):
-    cur.execute("INSERT INTO Dresseurs (id_dr, nom_dr, pokemon_id_dr, nom_pk) VALUES (?, ?, ?, ?)", 
-                (id_dr, nom_dr, id_pk, nom_pk))
-    cur.execute("DROP TABLE IF EXISTS Equipe")
-    cur.execute('''
-        CREATE TABLE Equipe AS
-        SELECT Dresseurs.nom_dr, Pokemons.nom_pk AS nom_pokemon
-        FROM Dresseurs
-        JOIN Pokemons ON Dresseurs.pokemon_id_dr = Pokemons.id_pk
-    ''')
-    conn.commit()
-    print(f"L'équipe du dresseur {nom_dr} à été mise à jour !")
-
+# FONCTIONS
 def afficher_equipe():
     commande = '''
     SELECT Dresseurs.nom_dr, Pokemons.nom_pk
@@ -102,6 +105,16 @@ def dresseur_puissant():
     for dresseur, pokemon, pk_niveau in resultat:
         print(f"Le dresseur {dresseur} est puissant : il possède un {pokemon} de niveau {pk_niveau} !!")
 
+def export_text_equipe():
+    cur.execute("SELECT nom_dr, nom_pokemon FROM Equipe")
+    donnees = cur.fetchall()
+    with open("equipe_pokemon.txt", "w", encoding="utf-8") as f:
+        f.write("=== RECAPITULATIF DES EQUIPES ===\n\n")
+        for dresseur, pokemon in donnees:
+            ligne = f"Dresseur {dresseur} combat avec {pokemon} !\n"
+            f.write(ligne)
+    print("Succès : le fichier 'equipe_pokemon.txt' a bien été généré !")
+
 
 print("------ POKEDEX ------")
 
@@ -121,7 +134,9 @@ affichage_recherche(resultat_poison)
 print("\n--- EQUIPES ---")
 afficher_equipe()
 
-print("\n--- DRESSEURS PUISSANTS ---")
+print("\n--- DRESSEURS PUISSANTS ---\n")
 dresseur_puissant()
 
 afficher_equipe()
+add_actu_equipe("Sacha", 5)
+export_text_equipe()
